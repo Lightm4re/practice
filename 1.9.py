2da317366e63b1b7826ae313d64484905644c1d3
Lightm4re <rmixa24@gmail.com>
2023-06-30 05:50:00 +0700

    Задача 1.9

---
+++ 1.9.py
@@ -0,0 +1,6 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+product = num1 * num2
+message = "Произведение чисел " + str(num1) + " и " + str(num2) + " равно " + str(product)
+print(message)
