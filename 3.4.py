c3b9a5054b8149f01faf9c80b01901a7ebb4f037
Lightm4re <rmixa24@gmail.com>
2023-07-02 00:06:20 +0700

    Задание 3.4

---
+++ 3.4.py
@@ -0,0 +1,10 @@
+import random
+
+length = int(input("Введите длину списка: "))
+start = int(input("Введите начальное значение диапазона: "))
+end = int(input("Введите конечное значение диапазона: "))
+
+num = [random.randint(start, end) for i in range(length)]
+print("Список чисел: \n", num)
+sqr = [i*i for i in num]
+print ("Список квадратов чисел: \n", sqr)
