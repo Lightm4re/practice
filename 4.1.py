5b125e614f32603b2b7a27cb4859cdb9b7eca821
Lightm4re <rmixa24@gmail.com>
2023-07-15 10:01:15 +0700

    Задание 4.1

---
+++ 4.1.py
@@ -0,0 +1,9 @@
+def get_largest_permutation(num):
+    num_str = str(num)
+    digits = sorted(num_str, reverse=True)
+    largest_permutation = int(''.join(digits))
+    return largest_permutation
+
+num = int(input("Введите число: "))
+largest_permutation = get_largest_permutation(num)
+print("Наибольшее число, полученное путем перестановки цифр числа", num, ":", largest_permutation)
