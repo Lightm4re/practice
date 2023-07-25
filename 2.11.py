c3ce9255d7ea1ef4fd0461a990f4170bdd5ae7f2
Lightm4re <rmixa24@gmail.com>
2023-07-25 23:26:38 +0700

    Доп. исправления во 2-ой части

--- 2.11.py
+++ 2.11.py
@@ -1,7 +1,7 @@
 a = int(input("Введите значение a: "))
 b = int(input("Введите значение b: "))
 
-for num in range(a, b+1):
+for num in range(a, b + 1):
     sum += num
 
 print("Сумма всех целых чисел от", a, "до", b, "включительно:", sum)
--- 2.4.py
+++ 2.4.py
@@ -2,14 +2,14 @@ a = float(input("Введите коэффициент a: "))
 b = float(input("Введите коэффициент b: "))
 c = float(input("Введите коэффициент c: "))
 
-discriminant = b**2 - 4*a*c
+discriminant = b ** 2 - 4 * a * c
 
 if discriminant < 0:
     print("Корней нет")
 elif discriminant == 0:
-    root = -b / (2*a)
+    root = - b / (2 * a)
     print("Корень уравнения:", root)
 else:
-    root1 = (-b + discriminant**0.5) / (2*a)
-    root2 = (-b - discriminant**0.5) / (2*a)
+    root1 = (- b + discriminant ** 0.5) / (2 * a)
+    root2 = (- b - discriminant ** 0.5) / (2 * a)
     print("Корни уравнения:", root1, "и", root2)
