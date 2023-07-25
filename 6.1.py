018b13f858eec6d5e2abc022791c20a5a905bd4c
Lightm4re <rmixa24@gmail.com>
2023-07-26 01:09:50 +0700

    Исправления 6-ой части

--- 6.1.py
+++ 6.1.py
@@ -1,18 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 def get_top_songs(url):
-    k=0
+    k = 0
     response = urlopen(url)
     soup = BeautifulSoup(response, 'html.parser')
     songsnames = soup.find_all(class_='d-track__name')
     if len(songsnames) > 0:
         for i in songsnames:
             print(i.text)
-            k+=1
-            if k==10:
+            k += 1
+            if k == 10:
                 break
     else:
         print("На указанной странице отсутствует информация о песнях")
+
+
 url = input("Введите ссылку на страницу исполнителя на Яндекс музыке: ")
 get_top_songs(url)
